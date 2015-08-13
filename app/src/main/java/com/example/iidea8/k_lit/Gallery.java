d8cd417678f6a6949afeded28eda841642fe7f88
curiosityExplo <abhigyan.the.one@gmail.com>
2015-08-13 18:33:28 +0530

    KLIT (updated Sponsors and Gallery) 8/13/15

--- app/src/main/java/com/example/iidea8/k_lit/Gallery.java
+++ app/src/main/java/com/example/iidea8/k_lit/Gallery.java
@@ -2,11 +2,14 @@ package com.example.iidea8.k_lit;
 
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 
 public class Gallery extends Fragment {
@@ -16,6 +19,23 @@ public class Gallery extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_gallery, container, false);
+        GridView gridview = (GridView) view.findViewById(R.id.gallery_gridView);
+        gridview.setAdapter(new GalleryAdapter(getActivity()));
+
+        // Listening to GridView item click
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                // Launch ImageViewPager.java on selecting GridView Item
+                Intent i = new Intent(getActivity().getApplicationContext(), GalleryViewPager.class);
+
+                // Send the click position to ImageViewPager.java using intent
+                i.putExtra("id", position);
+
+                // Start ImageViewPager
+                startActivity(i);
+            }
+        });
+        setRetainInstance(true);
         return view;
     }
 }
--- app/src/main/java/com/example/iidea8/k_lit/Sponsors.java
+++ app/src/main/java/com/example/iidea8/k_lit/Sponsors.java
@@ -7,15 +7,22 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 
 public class Sponsors extends Fragment {
     View view;
+    GridView gridView;
+    SponsorsAdapter sponsorsAdapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_sponsors, container, false);
+        gridView = (GridView) view.findViewById(R.id.gallery_grid_view);
+        sponsorsAdapter = new SponsorsAdapter(getActivity().getBaseContext());
+        gridView.setAdapter(sponsorsAdapter);
+        setRetainInstance(true);
         return view;
     }
 }
